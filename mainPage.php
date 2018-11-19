72aca124ea1d599c106d2429b2ceb6a4b443fbf1
quynhhnnguyen <quynhhnnguyen@users.noreply.github.com>
2018-11-19 14:31:30 -0700

    prepare for code merging to master branch

--- mainPage.php
+++ mainPage.php
@@ -1,5 +1,10 @@
 <!-- header -->
-<?php
+<?php 
+	/*
+		Author: Maryam Munir
+		Date created: Nov - 19 - 2018.
+		Summary: Home Page.
+	*/
 	include "header.php";
 
 	//set value to activeTab by session variable
@@ -8,83 +13,7 @@
 	//navigations
 	include "menu.php";
 ?>
-<!DOCTYPE html>
-<html>
-<head>
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<style>
-* {box-sizing: border-box;}
-body {font-family: Verdana, sans-serif;}
-.mySlides {display: none;}
-img {vertical-align: middle;}
 
-/* Slideshow container */
-.slideshow-container {
-  max-width: 100%;
-  position: relative;
-  margin: auto;
-}
-
-/* Caption text
-.text {
-  color: #f2f2f2;
-  font-size: 15px;
-  padding: 8px 12px;
-  position: absolute;
-  bottom: 8px;
-  width: 100%;
-  text-align: center;
-}*/
-
-/* Number text (1/3 etc)
-.numbertext {
-  color: #f2f2f2;
-  font-size: 12px;
-  padding: 8px 12px;
-  position: absolute;
-  top: 0;
-}*/
-
-/* The dots/bullets/indicators */
-.dot {
-  height: 15px;
-  width: 15px;
-  margin: 0 2px;
-  background-color: #bbb;
-  border-radius: 50%;
-  display: inline-block;
-  transition: background-color 0.6s ease;
-}
-
-.active {
-  background-color: #717171;
-}
-
-/* Fading animation */
-.fade {
-  -webkit-animation-name: fade;
-  -webkit-animation-duration: 1.5s;
-  animation-name: fade;
-  animation-duration: 1.5s;
-}
-
-@-webkit-keyframes fade {
-  from {opacity: .4}
-  to {opacity: 1}
-}
-
-@keyframes fade {
-  from {opacity: .4}
-  to {opacity: 1}
-}
-
-/* On smaller screens, decrease text size */
-@media only screen and (max-width: 300px) {
-  .text {font-size: 11px}
-}
-</style>
-</head>
-<body>
 
 <!-- Images Dimentions should be 1350x200 or little bit more otherwise
 it won't fit. It will take the whole page, So if anyone wants to change
@@ -93,15 +22,15 @@ the Images Please consider the dimentions Thanks-->
 <div class="slideshow-container">
 
 <div class="mySlides fade">
-   <img src="Images/France.jpg" style="width:80%;margin-left: 10%">
+   <img class="slideImg" src="img/France.jpg" style="width:80%;margin-left: 10%">
   </div>
 
 <div class="mySlides fade">
-  <img src="Images/Ibiza.jpg" style="width:80%;margin-left: 10%">
+  <img class="slideImg" src="img/Ibiza.jpg" style="width:80%;margin-left: 10%">
 </div>
 
 <div class="mySlides fade">
-  <img src="Images/Amsterdam.jpg" style="width:80%;margin-left: 10%">
+  <img class="slideImg" src="img/Amsterdam.jpg" style="width:80%;margin-left: 10%">
 </div>
 
 </div>
@@ -134,10 +63,6 @@ function showSlides() {
     setTimeout(showSlides, 2000); // Change image every 2 seconds
 }
 </script>
-
-</body>
-</html>
-
 
 
 <!-- footer -->
